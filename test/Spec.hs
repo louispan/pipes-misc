c2985b0b48d4f35c494b82470f8c48cc9e296a49
Louis Pan <louis@pan.me>
2018-04-09 21:18:41 +1000

    Comment out test for blocked thread which isn't handled in hspec 2.5

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Control.Concurrent
 import Control.Concurrent.STM
 import Control.Lens
 import Control.Monad.Morph
@@ -18,18 +19,21 @@ sig1 = traverse_ P.yield data1
 
 main :: IO ()
 main = do
-  hspec $ do
-      describe "Misc" $ do
-          it "Buffered" $ do
-              let xs = PP.toList (sig1 P.>-> PM.buffer 2 [])
-              xs `shouldBe` (pure $ head data1) : ((\(a, b) -> [a, b]) <$> zip (tail data1) data1)
+    hspec $ do
+        describe "Misc" $ do
+            it "Buffered" $ do
+                let xs = PP.toList (sig1 P.>-> PM.buffer 2 [])
+                xs `shouldBe` (pure $ head data1) : ((\(a, b) -> [a, b]) <$> zip (tail data1) data1)
 
-          it "Locally +10 to second" $ do
-              let xs = PP.toList
-                       (sig1
+            it "Locally +10 to second" $ do
+                let xs = PP.toList
+                        (sig1
                         P.>-> PP.map (\a -> (a, a))
                         P.>-> PM.locally (view _2) (set _2) (PP.map (+ 10)))
-              xs `shouldBe` zip data1 ((+ 10) <$> data1)
-          it "Filtering STM Producer blocks" $ do
-              sig <- PM.mkProducerSTM (PC.bounded 1) sig1
-              PP.toListM (hoist atomically (sig P.>-> PP.filter even)) `shouldThrow` anyException
+                xs `shouldBe` zip data1 ((+ 10) <$> data1)
+
+            -- -- hspec 2.5 doesn't work well with blocked threads, so comment this test out
+            -- -- jcristovao/enclosed-exceptions#12
+            -- it "Filtering STM Producer blocks" $ do
+            --     sig <- PM.mkProducerSTM (PC.bounded 1) sig1
+            --     PP.toListM (hoist atomically (sig P.>-> PP.filter even)) `shouldThrow` anyException
